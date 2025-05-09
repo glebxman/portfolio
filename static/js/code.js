2bd3d4d0d7784e61191bcfdd17ad3596eba3c231
20wrap <glebxxman@gmail.com>
2025-05-10 00:42:52 +0500

    first commit

---
+++ static/js/code.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const okno = document.getElementById('popup');
+    const openBtn = document.getElementById('openBtn');
+    const closeBtn = document.getElementById('closeBtn');
+
+    openBtn.addEventListener('click', () => {
+        okno.classList.remove('hidden');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        okno.classList.add('hidden');
+    });
+
+});
